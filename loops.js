9b01b762b6ef28b93c682429ff08399d3447303f
manojdev18 <110611278+manojdev18@users.noreply.github.com>
2023-09-28 10:27:19 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,57 @@
+// Example-1
+// To Print Numbers From 1 to 10 using while
+
+/*
+let endNumber = 10;
+let num = 1;
+
+while (num <= endNumber){
+    console.log(num);
+    num = num + 1;
+}
+*/
+// Example-2
+// To Print Numbers From 10 To 1 in a Decreasing Order 
+/*
+let endNumber = 1;
+let num = 10;
+
+while (num >= endNumber){
+    console.log(num);
+    num = num - 1;
+} 
+*/
+
+// Example-3 
+// Tabale of two print in while loop
+
+/*
+let endNumber = 10;
+let num = 1;
+while(num <= endNumber){
+    console.log(`2 * ${num} = ${2*num}`);
+    num = num + 1;
+}
+*/
+
+// Example-4
+// Write a program to print numbers 1 to 7 in a line using Do-while loop 
+/*
+let endNumber = 7;
+let num = 1;
+let result = "";
+do {
+    result = result + num;
+    num = num + 1;
+} while(num <= endNumber);
+    console.log(result);
+*/
+
+
+
+    
+
+
+
+
+
